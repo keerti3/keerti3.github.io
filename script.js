edd6017a6ff1e9fe72ea379602a127e7895b1ac2
Keerti <keertiaisham@gmail.com>
2024-11-10 22:54:44 -0500

    portfolio code

---
+++ script.js
@@ -0,0 +1,12 @@
+// scripts.js - For optional interactive features
+
+// Example: Smooth scrolling for links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
